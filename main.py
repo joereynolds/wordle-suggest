ddb28ea4f6951134ce659c9c0d5b42b984bc4ee6
Joe Reynolds <joereynolds952@gmail.com>
2023-07-26 21:19:01 +0100

    Weed out incorrectly position word suggestions

--- main.py
+++ main.py
@@ -43,6 +43,16 @@ def get_correctly_positioned_letters(parsed_board):
 
     return letters
 
+def get_incorrectly_positioned_correct_letters(parsed_board):
+    letters = {}
+    for line in parsed_board:
+        for i, letter in enumerate(line):
+            if '?' in letter:
+                letters[letter[0]] = i
+
+    return letters
+
+
 def is_invalid_letter_in_word(invalid_letters, word):
     for invalid_letter in invalid_letters:
         if invalid_letter.lower() in word:
@@ -64,35 +74,44 @@ def is_correctly_positioned_letter_in_word(correctly_positioned_letters, word):
 
     return False
 
+def is_correct_letter_in_wrong_position(almost_correct_letters, word):
+    for i in range(len(word)):
+        for letter, index in almost_correct_letters.items():
+            if word[i] == letter.lower() and i == index:
+                return True
+
 def suggest(parsed_board):
     board_word_limit = 5 + 1
     invalid_letters = get_invalid_letters(parsed_board)
     correct_letters = get_correctly_positioned_letters(parsed_board)
+    almost_correct_letters = get_incorrectly_positioned_correct_letters(parsed_board)
 
     suggestions = []
 
     for word in open('popular.txt'):
 
+        # Weed out anything not of valid length
         if len(word) != board_word_limit:
             continue
 
+        # Weed out stuff which has invalid letters in it
         if is_invalid_letter_in_word(invalid_letters, word):
+            continue
+
+        # Weed out words if valid letters in the wrong place
+        if is_correct_letter_in_wrong_position(almost_correct_letters, word):
             continue
 
         if is_correctly_positioned_letter_in_word(correct_letters, word):
             suggestions.append(word)
 
+
     return suggestions
-
-
 
 try:
     wordle_file = sys.argv[1]
 except IndexError:
     sys.exit("Please specify a txt file containing your wordle board so far")
-
-# print(parse_board(sys.argv[1]))
-# print(get_invalid_letters(parse_board(sys.argv[1])))
 
 suggestions = suggest(parse_board(sys.argv[1]))
 
